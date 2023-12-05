1f704292b57fb352e5367cac243da8cb73561d8a
Binh <nguyenducbinh2409@gmail.com>
2023-12-05 17:43:56 +0700

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs'; 
-import { Subscription } from 'rxjs'; 
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +12,17 @@ export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
   
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response: any) => {
-        this.users = response;
-      },
-      error: (error: any) => {
-        console.log(error);
-      }
-    });
-  }
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user') || null!); 
+    if (user) {
+      this.accountService.setCurrentUser(user);
+    }
+    }
+
 }
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  registerMode = false;
+ 
+
+  constructor() { }
+
+  ngOnInit():void {
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+    
+  
+
+  cancelRegisterModel(event: boolean){
+    this.registerMode = event;
+  }
+
+}
